80d221aeff16facaa76215f79cc12f0bf9b4683f
aplis1 <19170285+aplis1@users.noreply.github.com>
2025-01-02 11:23:16 -0500

    gatewayserver

--- accounts/src/main/java/com/msbytes/accounts/controller/CustomerController.java
+++ accounts/src/main/java/com/msbytes/accounts/controller/CustomerController.java
@@ -12,14 +12,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.constraints.Pattern;
 import org.apache.hc.core5.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(
         name = "CRUD REST APIs for Customers in ABCBank",
@@ -29,6 +28,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
 public class CustomerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
     private ICustomersService iCustomersService;
 
@@ -55,10 +56,12 @@ public class CustomerController {
     }
     )
     @GetMapping("/fetchCustomerDetails")
-    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestParam
-                                                                   @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestHeader("abcbank-correlation-id") String correlationId,
+                                                                    @RequestParam
+                                                                    @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                                                                        String mobileNumber){
-        CustomerDetailsDto customerDetailsDto = iCustomersService.fetchCustomerDetails(mobileNumber);
+        logger.debug("abcbank-correlation-id found: {} ", correlationId);
+        CustomerDetailsDto customerDetailsDto = iCustomersService.fetchCustomerDetails(mobileNumber, correlationId);
         return  ResponseEntity.status(HttpStatus.SC_OK).body(customerDetailsDto);
     }
 }
---
+++ gatewayserver/src/main/java/com/msbytes/gatewayserver/filters/RequestTraceFilter.java
@@ -0,0 +1,2 @@
+package com.msbytes.gatewayserver.filters;public class RequestTraceFilter {
+}
